8222d6764a92f90f71bdd9a4225d1ed4ca7955fd
favazquez <favazquez@uc.cl>
2016-05-14 19:40:44 -0300

    y
    g

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -5,5 +5,8 @@ skip_before_action :verify_authenticity_token
 	    head 400
 		  return
 		end
+    tag = params[:tag]
+  	access_token = params[:access_token]
+    tag_data= HTTParty.get('https://api.instagram.com/v1/tags/'+tag+'?access_token='+token)
   end
 end
