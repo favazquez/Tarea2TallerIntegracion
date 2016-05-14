3921e9406d73932ca2110f46a98cc46cbdce7966
favazquez <favazquez@uc.cl>
2016-05-14 20:05:41 -0300

    d
    d

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,5 +1,6 @@
 class InstagramController < ApplicationController
   require 'json'
+  require 'httparty'
   skip_before_action :verify_authenticity_token
   def search
     unless params[:tag] and params[:access_token]
