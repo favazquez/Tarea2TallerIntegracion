9edf73995c77473e7860ef2d6e213f55e4578e09
favazquez <favazquez@uc.cl>
2016-05-14 19:38:19 -0300

    ds
    c

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -3,7 +3,9 @@ skip_before_action :verify_authenticity_token
   def search
     unless params[:tag] and params[:access_token]
 	    head 400
+		  return
 		end
-  head 69
+    head 200
+    return
   end
 end
