cfe44e83960955a99e6f95fc0e92e29904ba2d52
favazquez <favazquez@uc.cl>
2016-05-14 18:25:33 -0300

    reset
    das

--- app/controllers/instagram_controller.rb
+++
@@ -1,8 +0,0 @@
-class InstagramController < ApplicationController
-  require 'json'
-  require 'httparty'
-  def search
-    unless params[:tag] and params[:access_token]
-      head 400
-  end
-end
