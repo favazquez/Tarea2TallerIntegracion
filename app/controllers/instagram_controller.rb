8e511fa31af39bd32f258083a8ac3522bc38631e
favazquez <favazquez@uc.cl>
2016-05-14 20:09:27 -0300

    c
    c

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -7,5 +7,8 @@ class InstagramController < ApplicationController
 	    head 400
 		  return
 		end
+    tag = params[:tag].to_s
+  	access_token = params[:access_token].to_s
+    tag_data= HTTParty.get('https://api.instagram.com/v1/tags/'+tag+'?access_token='+access_token)
   end
 end
